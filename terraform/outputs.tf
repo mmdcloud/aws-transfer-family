7e73801a43a05f8976242e715867910859342d36
mmdcloud <madmaxcloudpractice@gmail.com>
2025-12-15 08:46:34 +0530

    update: added key output for export and tested sftp upload

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "sftp_server_endpoint" {
 output "s3_bucket_id" {
   value = module.storage_bucket.id
 }
+
+output "sftp_private_key" {
+  value     = tls_private_key.tls_private_key.private_key_pem
+  sensitive = true
+}

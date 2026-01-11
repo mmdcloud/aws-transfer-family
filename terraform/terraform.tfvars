6c826bf88bb17ad49cf7c97245f5ff2f19985445
mmdcloud <madmaxcloudpractice@gmail.com>
2026-01-11 07:42:45 +0530

    update: added tfvars file

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+region = "us-east-1"
+sftp_user_name = "sftp-user"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,7 @@
 variable "region" {
   type    = string
-  default = "us-east-1"
 }
 
 variable "sftp_user_name" {
   type    = string
-  default = "sftp-user"
 }
